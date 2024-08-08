c9dcf91c03dee92f298bd9fc1f442e8b839c1360
Joshua DiChello <jddichello@gmail.com>
2024-08-07 23:05:55 -0400

    completed number analyzer lab

---
+++ Day-2-Sample-Conditional-Project/Day-2-Sample-Conditional-Project/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace Day_2_Sample_Conditional_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask the user for an odd number between 11 and 17
+            //Verify they have entered a correct value
+            //
+            //A number is odd if it is not evenly divisble by 2
+            //if we divide it by 2 and the remainder is not 0, its odd
+            //
+            //c# has modulus which provides the remainder after division
+
+            Console.Write("Please enter an odd number between 11 and 17: ");
+            int userInput = int.Parse(Console.ReadLine()); //Get user input and convert it to an int
+
+            //Show the user what we got from them
+            Console.WriteLine($"You entered { userInput }");
+
+            //Verify the number is between 11 and 17
+            if (userInput >= 11 && userInput <= 17 && userInput % 2 != 0)
+            {
+                Console.WriteLine("The value is odd and between 11 and 17");
+            }
+            else
+            {
+                Console.WriteLine("The value is not odd and between 11 and 17");
+            }
+
+            //Make visual studio keep the console window open till we are done
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ NumberAnalyzerLab/NumberAnalyzerLab/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NumberAnalyzerLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = 0;
+            bool isWithinRange = userInput >= 1 && userInput <= 100;
+
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+
+            Console.Clear();
+
+            while (true)
+            {
+                while (isWithinRange == false)
+                {
+                    Console.Clear();
+
+                    Console.Write($"{userName}, please enter a number between 1 and 100: ");
+                    bool isParsable = int.TryParse(Console.ReadLine(), out userInput);
+                    if (!isParsable) continue;
+
+                    Console.Clear();
+
+                    Console.WriteLine($"{userName}, you have entered {userInput}");
+
+                    if (userInput >= 1 && userInput <= 100) isWithinRange = true;
+                }
+
+                if (userInput % 2 != 0 && userInput < 60) Console.WriteLine($"{userName}, that number is odd and less than 60.");
+                if ((userInput % 2 == 0) && (userInput >= 2 && userInput <= 24)) Console.WriteLine($"{userName}, that number is even and less than 25.");
+                if ((userInput % 2 == 0) && (userInput >= 26 && userInput <= 60)) Console.WriteLine($"{userName}, that number is even and between 26 and 60 inclusive.");
+                if (userInput % 2 == 0 && userInput > 60) Console.WriteLine($"{userName}, that number is even and greater than 60.");
+
+                Console.WriteLine();
+                Console.WriteLine("Do you want to stop the program y/n: ");
+                string userChoice = Console.ReadLine();
+
+                if (userChoice.ToLower() == "y" && !string.IsNullOrWhiteSpace(userChoice)) break;
+                if (userChoice.ToLower() == "n" && !string.IsNullOrWhiteSpace(userChoice))
+                {
+                    userInput = 0;
+                    isWithinRange = false;
+                } 
+                Console.Clear();
+            }
+        }
+    }
+}
